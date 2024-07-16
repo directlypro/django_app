8978f72e314521f12262ce64990f1b42c734f1d0
directlypro <Providence.Mtsweni@snode.com>
2024-07-16 20:47:28 +0200

    my users model

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Users
 
 def users(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render())
+    myusers = Users.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+        'myusers' : myusers,
+    }
+    return HttpResponse(template.render(context, request))
